1a867da9ebe9dc079fe4446ffcf5035950c4a5f2
Chrys Bimokono <90347334+ChrysBimokono@users.noreply.github.com>
2022-05-24 15:14:12 -0400

    tomatoes app This is a react app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import "./styles.css";
-
+import React, { useState } from "react";
 // 1 -> display the counter number w/ state
 // 2 -> render the actual tomatoes 🍅
 // 3 -> refactor to create a TomatoBox component
 export default function App() {
+  const [counter, setCounter] = useState("");
+
   return (
     <div className="App">
       <div className="window">
         <h2>Tomato Counter</h2>
         <div className="tomato-box">
-          <h1>0</h1>
+          <h1>{counter ? counter : "No tomatoes yet"}</h1>
         </div>
         <div className="buttons-container">
-          <div className="button">-</div>
-          <div className="button">+</div>
+          <div
+            className="button"
+            onClick={() => setCounter(counter.slice(0, -2))}
+          >
+            -
+          </div>
+          <div className="button" onClick={() => setCounter(counter + "🍅")}>
+            +
+          </div>
         </div>
       </div>
     </div>
